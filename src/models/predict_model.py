2930e2c160095a40aca40dfd5c2fa8feac175724
Patrik Praprotnik <praprotnik.patrik@gmail.com>
2024-04-17 21:24:47 +0200

    Modified train_model and API for multivariat timeseries

--- src/models/predict_model.py
+++ src/models/predict_model.py
@@ -1,278 +1,100 @@
 import pandas as pd
-import numpy as np
-import glob
-import joblib
-import re
 import os
-import matplotlib.pyplot as plt
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.metrics import mean_squared_error, mean_absolute_error, explained_variance_score
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import SimpleRNN, Dense
+import numpy as np
+import joblib
+import requests
+import ast
+from tensorflow.keras.models import load_model
 
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
-processed_data_dir = os.path.join(current_dir, '..', '..', 'data', 'processed')
 models_dir = os.path.join(current_dir, '..', '..', 'models')
-reports_dir = os.path.join(current_dir, '..', '..', 'reports')
-window_size = 10 
-
-# ====================
-# IZRISOVANJE GRAFOV
-# ====================
-
-def plot_time_series(bike_data, selected_features):
-    for feature in selected_features:
-        plt.figure(figsize=(15, 6))
-        plt.plot(bike_data['datetime'], bike_data[feature])
-        plt.title(f'{feature} correlation with time')
-        plt.xlabel('Time')
-        plt.ylabel(f'{feature}')
-        plt.show()
-
-def plot_learning_history(history):
-    plt.plot(history.history['loss'], label='Train Loss')
-    plt.plot(history.history['val_loss'], label='Validation Loss')
-    plt.title('Training and Validation Loss')
-    plt.xlabel('Epochs')
-    plt.ylabel('Loss')
-    plt.legend()
-    plt.show()
-
-def plot_time_series_predictions(bike_data, predictions, mae, mse, evs):
-    plt.figure(figsize=(10, 5))
-    plt.plot(bike_data['datetime'], bike_data['available_bike_stands'], label='Resnična vrednost')
-    plt.plot(bike_data['datetime'][-len(predictions):], predictions, label='Napoved')
-    plt.title(f'MAE: {str(round(mae, 2))}, MSE: {str(round(mse, 2))}, EVS: {str(round(evs, 2))}')
-    plt.suptitle
-    plt.xlabel('Time')
-    plt.ylabel('available_bike_stands')
-    plt.legend()
-    plt.show()
-
-def plot_last_n_values(bike_data, predictions, n, mae, mse, evs):
-    plt.figure(figsize=(10, 5))
-    plt.plot(bike_data['datetime'][-n:], bike_data['available_bike_stands'][-n:], label='Resnična vrednost')
-    plt.plot(bike_data['datetime'][-n:], predictions[-n:], label='Napoved')
-    plt.title(f'MAE: {str(round(mae, 2))}, MSE: {str(round(mse, 2))}, EVS: {str(round(evs, 2))}')
-    plt.xlabel('Time')
-    plt.ylabel('available_bike_stands')
-    plt.legend()
-    plt.show()
+processed_data_dir = os.path.join(current_dir, '..', '..', 'data', 'processed')
+window_size = 5
 
 
-# ====================
-# OBDELAVA PODATKOV
-# ====================
+def fetch_fresh_weather_data(coordinates_lat, coordinates_lng):
+    url = f"https://api.open-meteo.com/v1/forecast?latitude={coordinates_lat}&longitude={coordinates_lng}&hourly=temperature_2m,relative_humidity_2m,dew_point_2m,apparent_temperature,precipitation_probability,rain,surface_pressure&forecast_days=1&timezone=auto"
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  # Raise exception for bad status codes
+        weather_data = response.json()
+        return weather_data
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching weather data: {e}")
+        return None
 
-def fill_missing_values(bike_data):
-    print("Features with missing values BEFORE:\n", bike_data.isnull().any(), "\n")
-    
-    # Split the dataset into two parts: one with missing values and one without
-    data_with_missing = bike_data[bike_data.isnull().any(axis=1)]
-    data_without_missing = bike_data.dropna()
-    columns_with_missing = bike_data.columns[bike_data.isnull().any()].tolist()
-    columns_without_missing = bike_data.columns[~bike_data.isnull().any()].tolist()
-    columns_without_missing = [col for col in columns_without_missing if col != 'date']
+def sortByDate(dataframe):
+    dataframe['datetime'] = pd.to_datetime(dataframe['datetime'])
+    dataframe = dataframe.sort_values('datetime')
+    return dataframe
 
-    for column in columns_with_missing:
-        X_train = data_without_missing[columns_without_missing]
-        y_train = data_without_missing[column]
-
-        # RandomForestRegressor had best results in the past
-        model = RandomForestRegressor(random_state=1234)
-        model.fit(X_train, y_train)
-
-        # Use the trained model to predict missing values
-        X_missing = data_with_missing[columns_without_missing]
-        predicted_values = model.predict(X_missing)
-
-        # Fill in missing values in the original dataset
-        bike_data.loc[bike_data.index.isin(data_with_missing.index), column] = predicted_values
-
-    print("Features with missing values AFTER:\n", bike_data.isnull().any(), "\n")
-    return bike_data
-
-# Function for creating parts / "choppings" of time series
-def create_time_series_data(time_series, window_size):
-    X, y = [], []
-    for i in range(len(time_series) - window_size):
-        window = time_series[i:(i + window_size), :]  # V okno vključimo vse značilnice
-        target = time_series[i + window_size, :]      # Target vključuje vse značilnice za naslednji časovni korak
-        X.append(window)
-        y.append(target)
-
-    X = np.array(X)
-    y = np.array(y)
-    return X, y[:, 0]
-
-# Function for preparing train and test data for multivariate time series
-def prepare_data_for_multivariate_time_series(bike_data, selected_features):
-    df_selected_features = bike_data[selected_features].values
-      
-    total_size = len(df_selected_features)          # Total size of dataframe
-    test_size = (total_size // 3) + window_size     # 33% of total_size + window_size
-    train_size = total_size - test_size
-
-    train_data, test_data = df_selected_features[:train_size], df_selected_features[train_size:]
-
-    # print('Train data length:', len(train_data))  
-    # print('Test data length:', len(test_data))    
-
-    # Split dataset in 2 parts - with and without target feature
-    available_bike_stands_train_data = train_data[:,0]
-    available_bike_stands_test_data = test_data[:,0]
-
-    other_train_data = train_data[:,1:]
-    other_test_data = test_data[:,1:]
-
-    # Normalize dataset
-    available_bike_stands_scaler = MinMaxScaler()
-    scaled_ABS_train_data = available_bike_stands_scaler.fit_transform(available_bike_stands_train_data.reshape(-1, 1))
-    scaled_ABS_test_data = available_bike_stands_scaler.transform(available_bike_stands_test_data.reshape(-1, 1))
-
-    features_scaler = MinMaxScaler()
-    scaled_other_train_data = features_scaler.fit_transform(other_train_data)
-    scaled_other_test_data = features_scaler.fit_transform(other_test_data)
-
-    # Combine all traning data
-    train_data = np.column_stack([
-        scaled_ABS_train_data,
-        scaled_other_train_data
-    ])
-
-    # Combine all test data
-    test_data = np.column_stack([
-        scaled_ABS_test_data,
-        scaled_other_test_data
-    ])
-
-    # Create time series parts of size 'window_size'
-    X_train, y_train = create_time_series_data(train_data, window_size)
-    X_test, y_test = create_time_series_data(test_data, window_size)
-
-    # print('Train data shape: X_train:', X_train.shape, ', y_train:', y_train.shape)
-    # print('Test data shape: X_test:', X_test.shape, ', y_test:', y_test.shape)
-
-    # Transpose X_train and X_test
-    X_train = np.transpose(X_train, (0, 2, 1))  # Transpose axes 1 and 2
-    X_test = np.transpose(X_test, (0, 2, 1))    # Transpose axes 1 and 2
-
-    # print('Reshaped X_train:', X_train.shape)
-    # print('Reshaped X_test:', X_test.shape)
-
-    return X_train, y_train, X_test, y_test, available_bike_stands_scaler, features_scaler
-
-
-# ========================================
-# IZGRADNJA IN UČENJE NAPOVEDNEGA MODELA
-# ========================================
-
-def build_model(inputShape):
-    model = Sequential()
-
-    model.add(SimpleRNN(64, activation='relu', input_shape=inputShape, return_sequences=True))
-    model.add(SimpleRNN(64, activation='relu'))
-
-    model.add(Dense(16, activation='relu'))
-    model.add(Dense(1))
-
-    model.compile(optimizer='adam', loss='mean_squared_error')
-    return model
-
-def evaluate_model(available_bike_stands_scaler, model, X_test, y_test):
-    # Predicting on test data
-    y_pred = model.predict(X_test)
-
-    # Inverse transformation of predicted and actual values
-    y_pred_inv = available_bike_stands_scaler.inverse_transform(y_pred)
-    y_test_inv = available_bike_stands_scaler.inverse_transform(y_test.reshape(-1, 1))
-
-    # Calculating metrics
-    mse = mean_squared_error(y_test_inv, y_pred_inv)
-    mae = mean_absolute_error(y_test_inv, y_pred_inv)
-    ev = explained_variance_score(y_test_inv, y_pred_inv)
-    return mse, mae, ev, y_pred_inv
-
-def save_data(station_number, model, available_bike_stands_scaler, features_scaler, history, mse, mae, ev):
-    dir_name = f'station{station_number}'
+def main(station_number):
     model_name = f'station{station_number}_model.h5'
-    train_metrics_name = f'station{station_number}_train_metrics.txt'
-    metrics_name = f'station{station_number}_metrics.txt'
-
-    # Save model
-    model.save(os.path.join(models_dir, dir_name, model_name))
-    print("Model saved!")
-
-    # Save scalers
-    joblib.dump(available_bike_stands_scaler, '../../models/ABS_scaler.pkl')
-    joblib.dump(features_scaler, '../../models/features_scaler.pkl')
-
-    # Save train metrics    
-    with open(os.path.join(reports_dir, train_metrics_name), "a") as file:
-        file.write(f"Loss: {history.history['loss']} \nVal_loss: {history.history['val_loss']}\n\n")
-    print(f"Train metrics for model {station_number} saved!")
-
-    # Save test metrics
-    with open(os.path.join(reports_dir, metrics_name), "a") as file:
-        file.write(f"MSE: {mse}\nMAE: {mae}\nEV: {ev} \n\n")
-    print(f"Test metrics {station_number} saved!")
-
-
-def main():
-    # Get a list of all CSV files in the directory
-    csv_files = glob.glob(os.path.join(processed_data_dir, 'station*_data.csv'))
-
-    # Select features for multivariate time series
-    selected_features = [
-        'available_bike_stands',
-        'temperature',
-        'relative_humidity',
-        'dew_point',
-        'apparent_temperature',
-        'precipitation_probability',
-        'rain',
-        'surface_pressure',
-        'bike_stands'
-    ]
     
-    for file in csv_files:
-        bike_data = pd.read_csv(file)
-        station_number = int(re.search('station(\d+)_data.csv', file).group(1))
+    model = load_model(os.path.join(models_dir, f'station{station_number}', model_name))
+    ABS_scaler = joblib.load(os.path.join(models_dir, f'station{station_number}', 'ABS_scaler.pkl'))
+    features_scaler = joblib.load(os.path.join(models_dir, f'station{station_number}', 'features_scaler.pkl'))
 
-        bike_data['datetime'] = pd.to_datetime(bike_data['datetime'])
-        bike_data = bike_data.sort_values(by='datetime')
-        bike_data = bike_data[
-            ['datetime'] +
-            ['available_bike_stands',
-            'temperature',
-            'relative_humidity',
-            'dew_point',
-            'apparent_temperature',
-            'precipitation_probability',
-            'rain',
-            'surface_pressure',
-            'bike_stands']
-        ]
-
-        if bike_data.isnull().any().any():
-            bike_data = fill_missing_values(bike_data)
-
-        X_train, y_train, X_test, y_test, available_bike_stands_scaler, features_scaler = prepare_data_for_multivariate_time_series(bike_data, selected_features)
-        
-        input_shape = (X_train.shape[1], X_train.shape[2])
-        print("Input shape: ", input_shape)
-        rnn_model = build_model(input_shape)
-        rnn_history = rnn_model.fit(X_train, y_train, epochs=30, validation_split=0.2)
-        mse, mae, ev, predictions_inv = evaluate_model(available_bike_stands_scaler, rnn_model, X_test, y_test)
-
-        # plot_learning_history(rnn_history)
-        # plot_time_series_predictions(bike_data, predictions_inv, mse, mae, ev)
-        # plot_last_n_values(bike_data, predictions_inv, 10, mse, mae, ev)
-
-        save_data(station_number, rnn_model, available_bike_stands_scaler, features_scaler, rnn_history, mse, mae, ev)
+    # Load existing station data
+    station_data = pd.read_csv(os.path.join(processed_data_dir, f'station{station_number}_data.csv'))
+    
+    # Fetch fresh weather data for given station
+    first_row = station_data.iloc[1]
+    location_dict = ast.literal_eval(first_row['coordinates'])
+    weather_data = fetch_fresh_weather_data(location_dict['lat'], location_dict['lng'])
 
 
-if __name__ == "__main__":
-    main()
+    # Select the last 'window_size' rows from station_data
+    last_rows = station_data.tail(window_size)
+
+    # Get the last datetime value from station_data
+    last_datetime = pd.to_datetime(last_rows['datetime'].values[-1])
+
+    # Convert the datetime values in weather_data to datetime objects
+    weather_datetimes = pd.to_datetime(weather_data['hourly']['time'])
+
+    # Find the index of the next datetime value in weather_data
+    next_datetime_index = weather_datetimes.searchsorted(last_datetime)
+
+    # If the next datetime value is not in weather_data, raise an error
+    if next_datetime_index == len(weather_datetimes):
+        raise ValueError("The next datetime value is not in weather_data")
+
+
+    # Select the next 7 values from each attribute in weather_data
+    next_values = {attr: weather_data['hourly'][attr][next_datetime_index:next_datetime_index+7] for attr in [
+        'time', 'temperature_2m', 'relative_humidity_2m', 'dew_point_2m', 'apparent_temperature', 
+        'precipitation_probability', 'rain', 'surface_pressure']}
+
+    # Create a new DataFrame with these values
+    new_rows = pd.DataFrame(next_values)
+
+    # Rename the columns to match the columns in station_data
+    new_rows.columns = ['datetime', 'temperature', 'relative_humidity', 'dew_point', 'apparent_temperature', 
+                        'precipitation_probability', 'rain', 'surface_pressure']
+
+    # Copy the 'number', 'name', 'address', 'coordinates', 'bike_stands' columns from last_rows
+    for col in ['number', 'name', 'address', 'coordinates', 'bike_stands']:
+        new_rows[col] = last_rows[col].values[0]
+
+    # Leave the 'available_bike_stands' column empty
+    new_rows['available_bike_stands'] = np.nan
+
+    # Append new_rows to last_rows
+    last_rows = last_rows.append(new_rows, ignore_index=True)
+
+    print(station_data.columns)
+    print(last_rows)
+
+
+
+
+    # available_bike_stands = bike_data['available_bike_stands'].values.reshape(-1, 1)
+    # n_available_bike_stands = standard_scaler.transform(available_bike_stands)
+    # n_available_bike_stands = np.reshape(n_available_bike_stands, (n_available_bike_stands.shape[1], 1, n_available_bike_stands.shape[0]))
+
+    # prediction = model.predict(n_available_bike_stands)[0]
+
+    # inversed_prediction = standard_scaler.inverse_transform(np.array(prediction).reshape(-1, 1))
+    return {"prediction": int("15")}
--- src/serve/api.py
+++ src/serve/api.py
@@ -1,67 +1,158 @@
 from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel, validator
+import uvicorn
+from pydantic import BaseModel
+from typing import List
+#from src.models.predict_model import predict_model
+import pandas as pd
 import os
 import numpy as np
-import pandas as pd
 import joblib
-import uvicorn
-from typing import List
+import requests
+import ast
 from tensorflow.keras.models import load_model
 
-# Get the directory of the current Python script
 current_dir = os.path.dirname(os.path.abspath(__file__))
-# Construct the path to the 'models' directory relative to the script's directory
 models_dir = os.path.join(current_dir, '..', '..', 'models')
-
-# Construct paths to individual model files within the 'models' directory
-model_path = os.path.join(models_dir, 'prediction_model.h5')
-scaler_path = os.path.join(models_dir, 'standard_scaler.pkl')
-
-model = load_model(model_path)
-standard_scaler = joblib.load(scaler_path)
-
-# Specify request body for vaja4 & vaja6
-class InputData(BaseModel):
-    data: List[dict]
-
-    @validator("data")
-    def validate_data(cls, value):
-        if not value or not all(isinstance(item, dict) for item in value):
-            raise ValueError("Invalid data format. Expecting a non-empty list of dictionaries.")
-        return value
-    
-def sortByDate(dataframe):
-    dataframe['date'] = pd.to_datetime(dataframe['date'])
-    dataframe = dataframe.sort_values('date')
-    return dataframe
+processed_data_dir = os.path.join(current_dir, '..', '..', 'data', 'processed')
+window_size = 20
+time_interval = 7
 
 # Create server
 app = FastAPI()
 
-@app.post("/mbajk/predict/", response_model=dict)
-async def predict_mBike(data: InputData):
+def fetch_fresh_weather_data(coordinates_lat, coordinates_lng):
+    url = f"https://api.open-meteo.com/v1/forecast?latitude={coordinates_lat}&longitude={coordinates_lng}&hourly=temperature_2m,relative_humidity_2m,dew_point_2m,apparent_temperature,precipitation_probability,rain,surface_pressure&forecast_days=2&timezone=auto"
     try:
-        input_data = pd.DataFrame(data.data)
-        print(input_data)
-        
-        # Validate DataFrame columns and required attributes
-        required_columns = ["date", "available_bike_stands"]
-        missing_columns = set(required_columns) - set(input_data.columns)
-        if missing_columns:
-            raise ValueError(f"Missing required columns: {missing_columns}")
-        
-        # Sort by date field
-        bike_data = sortByDate(input_data)
+        response = requests.get(url)
+        response.raise_for_status()  # Raise exception for bad status codes
+        weather_data = response.json()
+        return weather_data
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching weather data: {e}")
+        return None
+    
+def predict(station_number):
+    model_name = f'station{station_number}_model.h5'
+    
+    model = load_model(os.path.join(models_dir, f'station{station_number}', model_name))
+    ABS_scaler = joblib.load(os.path.join(models_dir, f'station{station_number}', 'ABS_scaler.pkl'))
+    features_scaler = joblib.load(os.path.join(models_dir, f'station{station_number}', 'features_scaler.pkl'))
 
-        
-        available_bike_stands = bike_data['available_bike_stands'].values.reshape(-1, 1)
-        n_available_bike_stands = standard_scaler.transform(available_bike_stands)
-        n_available_bike_stands = np.reshape(n_available_bike_stands, (n_available_bike_stands.shape[1], 1, n_available_bike_stands.shape[0]))
+    # Load existing station data
+    station_data = pd.read_csv(os.path.join(processed_data_dir, f'station{station_number}_data.csv'))
+    
+    # Fetch fresh weather data for given station
+    first_row = station_data.iloc[1]
+    location_dict = ast.literal_eval(first_row['coordinates'])
+    weather_data = fetch_fresh_weather_data(location_dict['lat'], location_dict['lng'])
 
-        prediction = model.predict(n_available_bike_stands)[0]
 
-        inversed_prediction = standard_scaler.inverse_transform(np.array(prediction).reshape(-1, 1))
-        return {"prediction": int(inversed_prediction)}
+    # Select the last 'window_size' rows from station_data
+    last_rows = station_data.tail(window_size)
+
+    # Get the last datetime value from station_data
+    last_datetime = pd.to_datetime(last_rows['datetime'].values[-1])
+
+    # Convert the datetime values in weather_data to datetime objects
+    weather_datetimes = pd.to_datetime(weather_data['hourly']['time'])
+
+    # Find the index of the next datetime value in weather_data
+    next_datetime_index = weather_datetimes.searchsorted(last_datetime) + 1
+
+    # If the next datetime value is not in weather_data, raise an error
+    if next_datetime_index == len(weather_datetimes):
+        raise ValueError("The next datetime value is not in weather_data")
+
+    # Select the next 'time_interval' values from each attribute in weather_data
+    next_values = {attr: weather_data['hourly'][attr][next_datetime_index:next_datetime_index + time_interval] for attr in [
+        'time', 'temperature_2m', 'relative_humidity_2m', 'dew_point_2m', 'apparent_temperature', 
+        'precipitation_probability', 'rain', 'surface_pressure']}
+
+    # Create a new DataFrame with these values
+    new_rows = pd.DataFrame(next_values)
+
+    # Rename the columns to match the columns in station_data
+    new_rows.columns = ['datetime', 'temperature', 'relative_humidity', 'dew_point', 'apparent_temperature', 
+                        'precipitation_probability', 'rain', 'surface_pressure']
+
+    # Copy the 'number', 'name', 'address', 'coordinates', 'bike_stands' columns from last_rows
+    for col in ['number', 'name', 'address', 'coordinates', 'bike_stands']:
+        new_rows[col] = last_rows[col].values[0]
+
+    # Convert 'datetime' to datetime objects
+    new_rows['datetime'] = pd.to_datetime(new_rows['datetime'])
+    # Format 'datetime' as 'YYYY-MM-DD HH:MM:SS'
+    new_rows['datetime'] = new_rows['datetime'].dt.strftime('%Y-%m-%d %H:%M:%S')
+
+    # Leave the 'available_bike_stands' column empty
+    new_rows['available_bike_stands'] = np.nan
+
+
+    # Concatenate last_rows and new_rows
+    dataset = pd.concat([last_rows, new_rows], ignore_index=True)
+
+    print(dataset)
+
+    # Initialize a list to store the predicted values
+    selected_features = [
+        'available_bike_stands',
+        'temperature',
+        'relative_humidity',
+        'dew_point',
+        'apparent_temperature',
+        'precipitation_probability',
+        'rain',
+        'surface_pressure',
+        'bike_stands'
+    ]
+    dataset = dataset[selected_features]
+    predicted_values = []
+    print("Dataset after:\n", dataset)
+
+    # Predict 7 steps ahead
+    for counter in range(7):
+        input_data = dataset[counter:window_size+counter].values
+
+        available_bike_stands_data = input_data[:, 0]
+        other_data = input_data[:, 1:]
+
+        scaled_ABS_data = ABS_scaler.transform(available_bike_stands_data.reshape(-1, 1))
+        scaled_other_data = features_scaler.transform(other_data)
+
+        complete_data = np.column_stack([
+            scaled_ABS_data,
+            scaled_other_data
+        ])
+        print("shape: ", complete_data.shape)
+        #complete_data_reshaped = np.transpose(complete_data, (0, 2, 1))    # Transpose axes 1 and 2
+        complete_data_reshaped = complete_data.reshape(1, complete_data.shape[1], complete_data.shape[0])    # Transpose axes 1 and 2
+
+        next_value = model.predict(complete_data_reshaped)
+        print("Next value: ", next_value)
+
+        # Add the predicted value to predicted_values
+        predicted_values.append(int(next_value[0][0]))
+
+        # Add the predicted value to input_data
+        dataset.at[window_size + counter, 'available_bike_stands'] = int(next_value[0][0])
+
+    print("Dataset after: ", dataset)    
+    print("Predicted values: ", predicted_values)
+    return predicted_values
+
+
+class StationData(BaseModel):
+    station_number: int
+
+class PredictionInput(BaseModel):
+    data: List[StationData]
+
+@app.post("/mbajk/predict/", response_model=dict)
+async def predict_mBike(input_data: PredictionInput):
+    station_number = input_data.data[0].station_number
+    try:   
+        result = predict(station_number)
+        return {'predictions': result} 
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
     
