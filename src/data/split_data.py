492bb94c7c796d5e48174db257596ec5aebf3255
Patrik Praprotnik <praprotnik.patrik@gmail.com>
2024-05-12 18:11:22 +0200

    Modified split_data.py script to split ALL data

--- src/data/split_data.py
+++ src/data/split_data.py
@@ -1,26 +1,33 @@
 import os
 import pandas as pd
 
-# Your existing code
 current_dir = os.path.dirname(os.path.abspath(__file__))
 data_dir = os.path.join(current_dir, '..', '..', 'data')
-current_dataset = pd.read_csv(os.path.join(data_dir, 'merged', 'current_data.csv'))
 
-# Convert 'datetime' column to datetime
-current_dataset['datetime'] = pd.to_datetime(current_dataset['datetime'])
+for i in range(1, 30):
+    # Define the current dataset file
+    station_data_filename = f'station{i}_data.csv'
+    station_data = pd.read_csv(os.path.join(data_dir, 'merged', station_data_filename))
 
-# Sort the data by 'datetime' column
-current_dataset = current_dataset.sort_values('datetime')
+    # Convert 'datetime' column to datetime
+    station_data['datetime'] = pd.to_datetime(station_data['datetime'])
 
-# Calculate the index at which to split the data
-split_index = int(len(current_dataset) * 0.9)
+    # Sort the data by 'datetime' column
+    station_data = station_data.sort_values('datetime')
 
-# Split the data into training and testing sets
-train_data = current_dataset.iloc[:split_index]
-test_data = current_dataset.iloc[split_index:]
+    # Calculate the index at which to split the data
+    split_index = int(len(station_data) * 0.9)
 
-# Save the training and testing data to CSV files
-train_data.to_csv(os.path.join(data_dir, 'merged', 'train_data.csv'), index=False)
-test_data.to_csv(os.path.join(data_dir, 'merged', 'test_data.csv'), index=False)
+    # Split the data into training and testing sets
+    train_data = station_data.iloc[:split_index]
+    test_data = station_data.iloc[split_index:]
 
-print("Data split and saved successfully!")
+    # Define the training and testing files
+    train_data_file = f'station{i}_train.csv'
+    test_data_file = f'station{i}_test.csv'
+
+    # Save the training and testing data to CSV files
+    train_data.to_csv(os.path.join(data_dir, 'merged', train_data_file), index=False)
+    test_data.to_csv(os.path.join(data_dir, 'merged', test_data_file), index=False)
+
+print("Data split and saved successfully for all stations!")
